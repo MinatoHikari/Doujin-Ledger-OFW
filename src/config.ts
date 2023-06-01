6a5cf90fc36b2d92980c64fda89bb7b614d9cb5e
MinatoHikari <35342316+MinatoHikari@users.noreply.github.com>
2023-06-02 03:06:29 +0800

    update version link

--- src/config.ts
+++ src/config.ts
@@ -40,6 +40,6 @@ export const SIDEBAR = [
 ];
 
 export const LastestRelease = {
-  url: "https://gitee.com/conomi/Doujin-Ledger-Release/releases/download/v1.0.0.beta-2/%E8%B1%86%E6%B4%A5%E8%B4%A6%E6%9C%ACv1.0.0.beta-2.apk",
+  url: "https://gitee.com/conomi/Doujin-Ledger-Release/releases/download/v1.0.0.beta-3/%E8%B1%86%E6%B4%A5%E8%B4%A6%E6%9C%ACv1.0.0.beta-3.apk",
   version: "1.0.1",
 };
