ced4263c46448c3b467078345eb534a5fe16420e
MinatoHikari <35342316+MinatoHikari@users.noreply.github.com>
2023-06-01 21:36:02 +0800

    remove api

--- api/latestVersion.ts
+++
@@ -1,9 +0,0 @@
-import { LastestRelease } from "../src/config";
-
-export const config = {
-  runtime: "serverless",
-};
-
-export default (request: Request) => {
-  return new Response(JSON.stringify(LastestRelease));
-};
