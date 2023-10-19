edb32d6f18e9ccc0dafe6cc474e914477488ae57
MinatoHikari <35342316+MinatoHikari@users.noreply.github.com>
2023-10-19 17:41:40 +0800

    1.5.3

--- src/config.ts
+++ src/config.ts
@@ -43,6 +43,6 @@ export const SIDEBAR = [
 ];
 
 export const LastestRelease = {
-  url: "https://gitee.com/conomi/Doujin-Ledger-Release/releases/download/v1.5.2/doujin-ledger-v1.5.2.apk",
-  version: "1.5.2",
+  url: "https://gitee.com/conomi/Doujin-Ledger-Release/releases/download/v1.5.3/doujin-ledger-v1.5.3.apk",
+  version: "1.5.3",
 };
