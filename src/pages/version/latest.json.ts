a78b596471866a9d388a5c2fb7f931cacf94ead5
MinatoHikari <35342316+MinatoHikari@users.noreply.github.com>
2025-11-25 11:35:41 +0800

    log 端点提供更多信息

--- src/pages/version/latest.json.ts
+++ src/pages/version/latest.json.ts
@@ -1,12 +1,15 @@
 import { LastestRelease } from "../../config";
+import { getVersionLog } from "../../config/versionLog";
 
 // endpoint https://docs.astro.build/en/guides/endpoints/
 
 export async function GET({ params, request }) {
+  const versionLog = getVersionLog(LastestRelease.version);
   return new Response(
     JSON.stringify({
       version: LastestRelease.version,
       url: LastestRelease.url,
+      text: versionLog?.changes || [],
     })
   );
 }
